407dc6fde48e6577da58d9680604839371c7cc76
Dhruv Jyoti Das <88588328+DhruvJyotiDas@users.noreply.github.com>
2025-05-10 19:16:26 +0530

    Update script.js

--- client/js/script.js
+++ client/js/script.js
@@ -9,12 +9,13 @@ const configuration = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     {
-      urls: 'turn:serveo.net:3478',
+      urls: 'turn:185.186.76.204:3478',
       username: 'test',
       credential: 'password'
     }
   ]
 };
+
 
 
 // DOM Elements
